51484c92a4b22e9baf6cc2c39a305c834deb991d
webaways <webaways@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-15 07:49:44 +0000

    Adding tinymce
    git-svn-id: https://plugins.svn.wordpress.org/nex-forms-express-wp-form-builder/trunk@893433 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tinyMCE/functions.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+function insert_a_form(){
+	
+	var shortcode  = '[NEXFormsExpress ';
+		shortcode += 'id="'+jQuery('select[name=wap_nex_forms_Id]').val() + '" ';
+		shortcode += ']';
+	
+	if(window.tinyMCE) {
+		//TODO: For QTranslate we should use here 'qtrans_textarea_content' instead 'content'
+		window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, shortcode);
+		//Peforms a clean up of the current editor HTML. 
+		//tinyMCEPopup.editor.execCommand('mceCleanup');
+		//Repaints the editor. Sometimes the browser has graphic glitches. 
+		tinyMCEPopup.editor.execCommand('mceRepaint');
+		tinyMCEPopup.close();
+	}
+	return;
+}
