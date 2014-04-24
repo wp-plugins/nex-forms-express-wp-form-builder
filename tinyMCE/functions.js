893436619ce155fac88f12aabb580882b7c747ef
webaways <webaways@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 16:17:43 +0000

    Update TinyMCE wp 3.9 insert into post problem
    git-svn-id: https://plugins.svn.wordpress.org/nex-forms-express-wp-form-builder/trunk@901807 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinyMCE/functions.js
+++ tinyMCE/functions.js
@@ -7,7 +7,13 @@ function insert_a_form(){
 	
 	if(window.tinyMCE) {
 		//TODO: For QTranslate we should use here 'qtrans_textarea_content' instead 'content'
-		window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, shortcode);
+		var tmce_ver=window.tinyMCE.majorVersion;
+
+		if (tmce_ver>="4") {
+        window.tinyMCE.execCommand('mceInsertContent', false, shortcode);
+			} else {
+				window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, shortcode);
+			}
 		//Peforms a clean up of the current editor HTML. 
 		//tinyMCEPopup.editor.execCommand('mceCleanup');
 		//Repaints the editor. Sometimes the browser has graphic glitches. 
